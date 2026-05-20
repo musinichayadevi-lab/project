fb96530c7366a4adfc687fc6713da1d763655c8f
musinichayadevi-lab <musinichayadevi@gmail.com>
2026-05-20 11:11:50 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,14 +1,22 @@
 package com.blooddonation.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mus.entity.UserEntity;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
-@WebServlet("/login")
+@WebServlet("/userlogin")
 public class LoginServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -16,31 +24,69 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	
 
-        // Get form data
+        // Get Form Data
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        // Dummy Login Validation
-        if(email.equals("admin@gmail.com") && password.equals("1234")) {
-
-            // Redirect to dashboard page
-            response.sendRedirect("dashboard.html");
-
+        // Check Empty Fields
+        if(email != null && password != null
+                && !email.isEmpty()
+                && !password.isEmpty()) {
+        	UserEntity loggedInUser = isValidUser(email, password);
+        	if(null != loggedInUser) {
+                HttpSession session = request.getSession();
+                session.setAttribute("loggedInUser", loggedInUser);
+                response.sendRedirect("new_user_dashboard.jsp");
+        	}
+            
         } else {
-
-            // Redirect back to login page
             response.setContentType("text/html");
-
+            response.getWriter().println( "<h2 style='color:red;text-align:center;'>Please Enter Email & Password ❌</h2>");
             response.getWriter().println(
-                "<h2 style='color:red;text-align:center;'>Invalid Email or Password ❌</h2>"
-            );
-
-            response.getWriter().println(
-                "<div style='text-align:center;'>"
-                + "<a href='login.html'>Try Again</a>"
-                + "</div>"
+                    "<div style='text-align:center;'>"
+                    + "<a href='userLogin.html'>Try Again</a>"
+                    + "</div>"
             );
         }
+    }
+    
+    public UserEntity isValidUser(String email, String password) {
+    	UserEntity user = new UserEntity();
+    	// Database connection details
+    	String url = "jdbc:mysql://localhost:3306/blood_donation";
+        String db_user = "root";
+        String db_password = "root";
+
+        // SQL query using placeholders (?) to prevent SQL injection
+        String sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+
+        try{
+        	Class.forName("com.mysql.cj.jdbc.Driver");
+        	Connection conn = DriverManager.getConnection(url, db_user, db_password);
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            // Set values for the placeholders
+            pstmt.setString(1, email);
+            pstmt.setString(2, password);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                // If the ResultSet has at least one row, the user is valid
+                if (rs.next()) {
+                	int id = rs.getInt("id");
+                    String username = rs.getString("name");
+                    email = rs.getString("email");
+                    String role = rs.getString("role");
+                    
+                    return new UserEntity(id, username, email,"",role);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return user;
     }
 }
